2de5489554bcacb48fb623da9afd135332779538
Andy <andy@MacBooks-MacBook-Pro.local>
2020-06-01 15:19:42 +0800

    Updates

---
+++ precache-manifest.382627449c3062be5c9aa80d80c9977e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d5bfdece17ccd22e128614bd60f54d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08bb9ce6329f8f900105",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c72bfc57083463afdec2",
+    "url": "/static/css/main.03b4620a.chunk.css"
+  },
+  {
+    "revision": "08bb9ce6329f8f900105",
+    "url": "/static/js/2.5941c06e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5941c06e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c72bfc57083463afdec2",
+    "url": "/static/js/main.a4bebb95.chunk.js"
+  },
+  {
+    "revision": "bb20e95e617ff8a931e5",
+    "url": "/static/js/runtime-main.26020ee8.js"
+  },
+  {
+    "revision": "1539fa5748edf59cbf232e3ea435fda6",
+    "url": "/static/media/1001.1539fa57.jpg"
+  },
+  {
+    "revision": "1534c150914568ff0f7e8e7c2447c1dd",
+    "url": "/static/media/1001_thumb.1534c150.jpg"
+  },
+  {
+    "revision": "c7487b148cb5350e0ceeaff548167b00",
+    "url": "/static/media/1002.c7487b14.jpg"
+  },
+  {
+    "revision": "65ce0840d53e6b4b27c1b7603330ebfa",
+    "url": "/static/media/1002_thumb.65ce0840.jpg"
+  },
+  {
+    "revision": "40c062a03df6761cb65b7f62c27e9be1",
+    "url": "/static/media/1003.40c062a0.jpg"
+  },
+  {
+    "revision": "ead585afa0850bcac85a8cc734b47dac",
+    "url": "/static/media/1003_thumb.ead585af.jpg"
+  },
+  {
+    "revision": "806eda6eab77091d79cdb260e7631855",
+    "url": "/static/media/1004.806eda6e.jpg"
+  },
+  {
+    "revision": "2f12a9783a2ae6d1d320708904650079",
+    "url": "/static/media/1004_thumb.2f12a978.jpg"
+  },
+  {
+    "revision": "40ae90b4c5bd1be958650a76b324e8d6",
+    "url": "/static/media/adrenalin.40ae90b4.svg"
+  }
+]);
